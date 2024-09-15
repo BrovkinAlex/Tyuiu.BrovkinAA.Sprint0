ce935df3757ed3684056920730c1a8bf4bc49bbc
BrovkinAlex <Screep_dogg@mail.ru>
2024-09-15 16:51:39 +0300

    Task4

---
+++ Tyuiu.BrovkinAA.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+using Tyuiu.BrovkinAA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.BrovkinAA.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekedAdditionlValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+        public void ChekedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        [TestMethod]
+        public void ChekedDivisionValid()
+        {
+            Assert.AreEqual(7, DataService.Division(21, 3));
+        }
+
+        [TestMethod]
+        public void ChekedMultiplicationValid()
+        {
+            Assert.AreEqual(130, DataService.Multiplication(13, 10));
+        }
+    }
+}
---
+++ Tyuiu.BrovkinAA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BrovkinAA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.BrovkinAA.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(3, 7));
+            Console.WriteLine(DataService.Subtraction(8, 2));
+            Console.WriteLine(DataService.Multiplication(4, 4));
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
