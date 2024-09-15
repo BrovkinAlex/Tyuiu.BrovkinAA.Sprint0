bbab55713272ef61d44ac620345d74230d569a4e
BrovkinAlex <Screep_dogg@mail.ru>
2024-09-15 16:56:10 +0300

    Task4

--- Tyuiu.BrovkinAA.Sprint0.Task4.V0/Program.cs
+++ Tyuiu.BrovkinAA.Sprint0.Task4.V0/Program.cs
@@ -6,9 +6,9 @@ namespace Tyuiu.BrovkinAA.Sprint0.Task4.V0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DataService.Addition(3, 7));
-            Console.WriteLine(DataService.Subtraction(8, 2));
-            Console.WriteLine(DataService.Multiplication(4, 4));
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(10, 10));
             Console.WriteLine(DataService.Division(5, 5));
 
             Console.ReadKey();
