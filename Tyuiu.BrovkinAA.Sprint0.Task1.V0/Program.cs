30c7d0db3bdef6b7c3fa6410ea7400ae59464005
BrovkinAlex <Screep_dogg@mail.ru>
2024-09-15 14:10:15 +0300

    Task1

---
+++ Tyuiu.BrovkinAA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.BrovkinAA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Алексей";
+            int age = 22;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " года!");
+            Console.ReadKey();
+        }
+    }
+}
